feaa25090c2370afc0546c0fa35937738f195cea
HI andres <HI andres@NALECOMPUTERS1>
2020-05-12 07:23:42 -0500

    actualizacion del proyecto

--- src/ec/edu/ups/dao/UsuarioDAO.java
+++ src/ec/edu/ups/dao/UsuarioDAO.java
@@ -6,11 +6,65 @@
 package ec.edu.ups.dao;
 
 import ec.edu.ups.idao.IUsuario;
+import ec.edu.ups.modelo.Usuario;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  *
  * @author HI andres
  */
 public class UsuarioDAO implements IUsuario {
-    
+
+     private List<Usuario> listaUsuario;
+
+    public UsuarioDAO() {
+        listaUsuario = new ArrayList<>();
+    }
+
+    @Override
+    public void create(Usuario usuario) {
+        listaUsuario.add(usuario);
+    }
+
+    @Override
+    public Usuario read(String cedula) {
+        for (Usuario usuario : listaUsuario) {
+            if (usuario.getCedula().equals(usuario.getCedula())) {
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Usuario usuario) {
+        for (int i = 0; i < listaUsuario.size(); i++) {
+            Usuario usu = listaUsuario.get(i);
+            if (usu.getCedula().equals(usuario.getCedula())) {
+                listaUsuario.set(i, usuario);
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(Usuario usuario) {
+        Iterator<Usuario> it = listaUsuario.iterator();
+        while (it.hasNext()) {
+            Usuario usu = it.next();
+            if (usu.getCedula().equals(usuario.getCedula())) {
+                it.remove();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        return listaUsuario;
+    }
+
 }
