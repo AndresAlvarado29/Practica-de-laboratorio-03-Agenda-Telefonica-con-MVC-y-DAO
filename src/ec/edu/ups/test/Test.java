ff48e3761690dd1f8f85287e72ca443574881d94
HI andres <HI andres@NALECOMPUTERS1>
2020-05-11 22:48:33 -0500

    actualizacion del proyecto

---
+++ src/ec/edu/ups/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HI andres
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner escribir = new Scanner(System.in);
+        System.out.println("********Agenda Telefónica********");
+        System.out.println("          ¡Bienvenido!         "+"\n");
+        System.out.println("[1]Iniciar sesión"+"  "+"[2]Registrarse");
+        System.out.println("[3]Salir");
+        
+    }
+    
+}
---
+++ src/ec/edu/ups/vista/VistaUsuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.vista;
+
+import ec.edu.ups.modelo.Usuario;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HI andres
+ */
+public class VistaUsuario {
+    private Scanner escribir;
+
+    public VistaUsuario() {
+  escribir = new Scanner (System.in);
+    }
+public Usuario crearUsuario(){
+return null;
+}
+}
